cdd62f9229ada8c26e2c750761ab2fddfcfb59e8
Mateus Henrique <contato.henriqm@gmail.com>
2021-10-26 15:16:16 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function menuToggle() {
+    let menuArea = document.getElementById("menu-area");
+
+    if(menuArea.style.width == '200px') {
+        menuArea.style.width == '0px';
+    } else {
+        menuArea.style.width == '200px';
+    }
+   
+
+}
